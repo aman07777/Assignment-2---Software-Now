fa76641e356bae7317b042d257fc10d85140b028
aman07777 <amankoirala97@gmail.com>
2026-01-10 22:36:58 +1100

    questions of assignment

---
+++ question_2.py
@@ -0,0 +1,5 @@
+"""Create a program that analyses temperature data collected from multiple
+ weather stations in Australia. The data is stored in multiple CSV files
+under a "temperatures" folder, with each file representing data from one 
+year. Process ALL .csv files in the temperatures folder.Ignore
+ missing temperature values (NaN) in calculations."""
---
+++ question_3.py
@@ -0,0 +1,3 @@
+"""Create a program that uses a recursive function to generate a geometric pattern using 
+Python's turtle graphics. The pattern starts with a regular polygon and recursively 
+modifies each edge to create intricate designs. """
